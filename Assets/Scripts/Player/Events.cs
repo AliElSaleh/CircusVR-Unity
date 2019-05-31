708ce2c57cbb810400da9fe816b247ec7e63daed
Ali <elsaleh78@gmail.com>
2019-05-31 16:53:29 +1000

    Implement pointer for picking up objects

--- Assets/Scripts/Player/Events.cs
+++ Assets/Scripts/Player/Events.cs
@@ -1,20 +1,128 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using JetBrains.Annotations;
+using UnityEngine.Events;
 
 namespace Assets.Scripts.Player
 {
     public class Events : MonoBehaviour
     {
+        public static UnityAction OnTouchpadUp = null;
+        public static UnityAction OnTouchpadDown = null;
+        public static UnityAction<OVRInput.Controller, GameObject> OnControllerSource;
+
+        public GameObject RightAnchor;
+        public GameObject HeadAnchor;
+
+        private Dictionary<OVRInput.Controller, GameObject> ControllerSets = null;
+        private OVRInput.Controller InputSource = OVRInput.Controller.None;
+        private OVRInput.Controller Controller = OVRInput.Controller.None;
+        private bool InputActive = true;
+
+        private string Log;
+
+        private void Awake()
+        {
+            OVRManager.HMDMounted += PlayerFound;
+            OVRManager.HMDUnmounted += PlayerLost;
+
+            ControllerSets = CreateControllerSets();
+        }
+
+        private void OnDestroy()
+        {
+            OVRManager.HMDMounted -= PlayerFound;
+            OVRManager.HMDUnmounted -= PlayerLost;
+        }
+
         [UsedImplicitly]
         private void Start()
         {
-		
         }
 	
         [UsedImplicitly]
         private void Update()
         {   
+            // Check for active input
+            if (!InputActive)
+                return;
 
+            CheckForController();
+
+            CheckInputSource();
+
+            Input();
+        }
+
+        private void CheckInputSource()
+        {
+            InputSource = UpdateSource(OVRInput.GetActiveController(), InputSource);
+        }
+
+        private void Input()
+        {
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                if (OnTouchpadDown != null)
+                    OnTouchpadDown();
+            }
+
+            if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                if (OnTouchpadUp != null)
+                    OnTouchpadUp();
+            }
+        }
+
+        private OVRInput.Controller UpdateSource(OVRInput.Controller Check, OVRInput.Controller Previous)
+        {
+            // If values are the same, return
+            if (Check == Previous)
+                return Previous;
+
+            // Get Controller object
+            GameObject ControllerObject;
+            ControllerSets.TryGetValue(Check, out ControllerObject);
+
+            // Send out event
+            if (OnControllerSource != null)
+                OnControllerSource(Check, ControllerObject);
+
+            // Return new value
+            return Check;
+        }
+
+        private void CheckForController()
+        {
+            OVRInput.Controller ControllerCheck = Controller;
+
+            // Right remote
+            if (OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote))
+                ControllerCheck = OVRInput.Controller.RTrackedRemote;
+
+            Controller = UpdateSource(ControllerCheck, Controller);
+        }
+
+        private void PlayerFound()
+        {
+            InputActive = true;
+            Log = "Player Found";
+        }
+
+        private void PlayerLost()
+        {
+            InputActive = false;
+            Log = "Player Lost";
+        }
+
+        private Dictionary<OVRInput.Controller, GameObject> CreateControllerSets()
+        {
+            Dictionary<OVRInput.Controller, GameObject> NewSets = new Dictionary<OVRInput.Controller, GameObject>()
+            {
+                {OVRInput.Controller.RTrackedRemote, RightAnchor}
+            };
+
+            return NewSets;
         }
     }
 }
---
+++ Assets/Scripts/Player/Pointer.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class Pointer : MonoBehaviour
+    {
+        public float Distance = 10.0f;
+        public LineRenderer LineRenderer = null;
+        public LayerMask EverythingLayerMask;
+        public LayerMask InteractableMask = 0;
+
+        private Transform CurrentOrigin = null;
+
+        private void Awake()
+        {
+            Events.OnControllerSource += UpdateOrigin;
+            Events.OnTouchpadDown += ProcessTouchpadDown;
+        }
+
+        private void OnDestroy()
+        {
+            Events.OnControllerSource -= UpdateOrigin;
+            Events.OnTouchpadDown -= ProcessTouchpadDown;
+        }
+
+        private void Start()
+        {
+            SetLineColor();
+        }
+
+        private void Update()
+        {
+            Vector3 HitPoint = UpdateLine();
+        }
+
+        private Vector3 UpdateLine()
+        {
+            // Create ray
+            RaycastHit Hit = CreateRaycast(EverythingLayerMask);
+
+            // Line end
+            Vector3 EndLocation = CurrentOrigin.position + (CurrentOrigin.forward * Distance);
+
+            // Check hit
+            if (Hit.collider != null)
+                EndLocation = Hit.point;
+
+            // Set position
+            LineRenderer.SetPosition(0, CurrentOrigin.position);
+            LineRenderer.SetPosition(1, EndLocation);
+
+            return EndLocation;
+        }
+
+        private RaycastHit CreateRaycast(int Layer)
+        {
+            RaycastHit Hit;
+            Ray Ray = new Ray(CurrentOrigin.position, CurrentOrigin.forward);
+            Physics.Raycast(Ray, out Hit, Layer);
+
+            return Hit;
+        }
+
+        private void SetLineColor()
+        {
+            if (!LineRenderer)
+                return;
+
+            Color EndColor = Color.white;
+            EndColor.a = 0.0f;
+
+            LineRenderer.endColor = EndColor;
+        }
+
+        private void UpdateOrigin(OVRInput.Controller Controller, GameObject ControllerObject)
+        {
+            // Set origin of pointer
+            CurrentOrigin = ControllerObject.transform;
+
+            // Is the laser visible?
+            if (Controller == OVRInput.Controller.Touchpad)
+            {
+                LineRenderer.enabled = false;
+            }
+            else
+            {
+                LineRenderer.enabled = true;
+            }
+        }
+
+        private void ProcessTouchpadDown()
+        {
+
+        }
+    }
+}
